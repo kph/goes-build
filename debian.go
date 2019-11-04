e9d2daf48fe735e724e02e8f7b43f5120d35669a
Kevin Paul Herbert <kph@platinasystems.com>
2019-11-04 18:55:05 -0500

    debian: checkpoint common writer

--- debian.go
+++ debian.go
@@ -44,6 +44,43 @@ type nb struct {
 	Body []byte
 }
 
+func newTarMember(aw *ar.Writer, name string, data []nb) (err error) {
+	var databuf bytes.Buffer
+	twz := gzip.NewWriter(&databuf)
+	tw := tar.NewWriter(twz)
+
+	for _, entry := range data {
+		hdr := &tar.Header{
+			Name: entry.Name,
+			Mode: 0600,
+			Size: int64(len(entry.Body)),
+		}
+		if err := tw.WriteHeader(hdr); err != nil {
+			return fmt.Errorf("Error writing header for %s in %s: %w",
+				entry.Name, name, err)
+		}
+		cnt, err := tw.Write(entry.Body)
+		if err != nil {
+			return fmt.Errorf("Error writing data for %s in %s: %w",
+				entry.Name, name, err)
+		}
+		if cnt != len(entry.Body) {
+			return fmt.Errorf("Error writing data for %s in %s: wrote %d expecting %d",
+				entry.Name, name, cnt, len(entry.Body))
+		}
+	}
+	if err := tw.Close(); err != nil {
+		return fmt.Errorf("Error closing tar %s: %w", name, err)
+	}
+	if err := twz.Close(); err != nil {
+		return fmt.Errorf("Error closing %s: %w", name, err)
+	}
+	if err := sliceToDeb(aw, name, databuf.Bytes()); err != nil {
+		return err
+	}
+	return nil
+}
+
 func NewDebianArchive(name string, data []nb) (err error) {
 	deb, err := os.Create(name)
 	if err != nil {
@@ -54,7 +91,7 @@ func NewDebianArchive(name string, data []nb) (err error) {
 	aw.WriteGlobalHeader()
 	sliceToDeb(aw, "debian-binary", []byte("2.0\n"))
 
-	control := fmt.Sprintf(`Package: %s
+	controlFile := fmt.Sprintf(`Package: %s
 Version: %s
 Maintainer: %s
 Description: Package short description will go here
@@ -73,20 +110,20 @@ Architecture: amd64
 	hdr := &tar.Header{
 		Name: "control",
 		Mode: 0600,
-		Size: int64(len(control)),
+		Size: int64(len(controlFile)),
 	}
 	if err := tw.WriteHeader(hdr); err != nil {
 		return fmt.Errorf("Error writing header for control in control.tar.gz: %w",
 			err)
 	}
-	cnt, err := tw.Write([]byte(control))
+	cnt, err := tw.Write([]byte(controlFile))
 	if err != nil {
 		return fmt.Errorf("Error writing data for control in control.tar: %w",
 			err)
 	}
-	if cnt != len(control) {
+	if cnt != len(controlFile) {
 		return fmt.Errorf("Error writing data for control in control.tar: wrote %d expecting %d",
-			cnt, len(control))
+			cnt, len(controlFile))
 	}
 	if err := tw.Close(); err != nil {
 		return fmt.Errorf("Error closing control.tar: %w", err)
@@ -98,39 +135,8 @@ Architecture: amd64
 		return err
 	}
 
-	var databuf bytes.Buffer
-	twz = gzip.NewWriter(&databuf)
-	tw = tar.NewWriter(twz)
+	newTarMember(aw, "data.tar.gz", data)
 
-	for _, entry := range data {
-		hdr := &tar.Header{
-			Name: entry.Name,
-			Mode: 0600,
-			Size: int64(len(entry.Body)),
-		}
-		fmt.Printf("Writing %s data %s\n", entry.Name, string(entry.Body))
-		if err := tw.WriteHeader(hdr); err != nil {
-			return fmt.Errorf("Error writing header for %s in data.tar: %w", entry.Name, err)
-		}
-		cnt, err := tw.Write(entry.Body)
-		if err != nil {
-			return fmt.Errorf("Error writing data for %s in data.tar: %w",
-				entry.Name, err)
-		}
-		if cnt != len(entry.Body) {
-			return fmt.Errorf("Error writing data for %s in data.tar: wrote %d expecting %d",
-				entry.Name, cnt, len(entry.Body))
-		}
-	}
-	if err := tw.Close(); err != nil {
-		return fmt.Errorf("Error closing data.tar: %w", err)
-	}
-	if err := twz.Close(); err != nil {
-		return fmt.Errorf("Error closing data.tar.gz: %w", err)
-	}
-	if err := sliceToDeb(aw, "data.tar.gz", databuf.Bytes()); err != nil {
-		return err
-	}
 	if err := deb.Close(); err != nil {
 		return fmt.Errorf("Error closing archive %s: %w", name, err)
 	}
